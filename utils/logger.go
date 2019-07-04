16de70ed1d1d861dc759c62faf14696d29c47151
HaoDaWang <1306036576@qq.com>
2019-07-04 11:59:03 +0800

    [Feat] add version

--- utils/logger.go
+++ utils/logger.go
@@ -8,3 +8,13 @@ import (
 func Log(msg string){
 	fmt.Printf("[%s]: %s", time.Now().Format("2006-01-02 15:04:05"), msg)
 }
+
+func Version(o bool) string {
+	version := "0.0.1"
+
+	if o {
+		fmt.Println(version)
+	}
+
+	return version
+}
