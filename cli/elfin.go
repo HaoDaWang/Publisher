f748de188ff81f742ecae24285adea8db8b27e7d
HaoDaWang <1306036576@qq.com>
2019-07-05 14:43:43 +0800

    [Feat] exec

--- cli/elfin.go
+++ cli/elfin.go
@@ -1,11 +1,26 @@
 package cli
 
+import (
+	"log"
+	"os/exec"
+	"strings"
+)
+
 const (
 	TEST_ENV_1 = iota
 	TEST_ENV_2
 	LOCAL_TEST_ENV
 )
 
-func publish(branchName string, env int){
+func Publish(branchName string, env int) []string {
+	cmd := exec.Command("yarn", "--help")
+	out, err := cmd.Output()
 
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	outs := strings.Split(string(out), "\n")
+
+	return outs
 }
