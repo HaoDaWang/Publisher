ee82f405a4edd041a9eeeb2336694379917da822
HaoDaWang <1306036576@qq.com>
2019-07-05 14:43:21 +0800

    [Feat] add router

--- main.go
+++ main.go
@@ -8,12 +8,17 @@ import (
 func main(){
 	mux := http.NewServeMux()
 
+	mux.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
 	mux.HandleFunc("/", routes.Index)
+	mux.HandleFunc("/task/add", routes.AddTask)
+	mux.HandleFunc("/task/all", routes.GetTasks)
+	mux.HandleFunc("/task/remove", routes.RemoveTask)
+	mux.HandleFunc("/branches", routes.GetBranches)
 
 	server := Server{
-		Host:"0.0.0.0",
+		Host: "0.0.0.0",
 		Port: "3000",
-		Handler:mux,
+		Handler: mux,
 	}
 
 	server.listen()
