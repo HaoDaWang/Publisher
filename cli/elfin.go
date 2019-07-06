1df6bd9663253ac6e39100cdcb4a36953ab9b940
HaoDaWang <1306036576@qq.com>
2019-07-06 13:54:02 +0800

    [Feat] add publish cli

--- cli/elfin.go
+++ cli/elfin.go
@@ -1,8 +1,11 @@
 package cli
 
 import (
+	"bytes"
+	"fmt"
 	"log"
 	"os/exec"
+	"publisher/utils"
 	"strings"
 )
 
@@ -12,12 +15,38 @@ const (
 	LOCAL_TEST_ENV
 )
 
+var elfinCli = map[int]string{
+	TEST_ENV_2: "testing2",
+	TEST_ENV_1: "testing1",
+	LOCAL_TEST_ENV: "local",
+}
+
+func Command(name string, arg ...string) {
+	fmt.Println(append([]string{name}, arg...))
+}
+
 func Publish(branchName string, env int) []string {
-	cmd := exec.Command("yarn", "--help")
+	cmd := exec.Command(
+		"elfin",
+		"deploy",
+		"--local",
+		"-e",
+		elfinCli[env],
+		"-b",
+		branchName,
+	)
+
+	var stderr bytes.Buffer
+
+	cmd.Stderr = &stderr
+	cmd.Dir = utils.GetProjectName()
+
 	out, err := cmd.Output()
 
+	//fmt.Println(utils.GetProjectName())
+
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(stderr.String(), err)
 	}
 
 	outs := strings.Split(string(out), "\n")
